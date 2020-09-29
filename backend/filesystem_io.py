774b204d34a26da405812aa31fe73ed31af98a03
Neil <neil.dutoit@qdivision.co>
2020-09-29 11:26:50 +0200

    fixed filename extension duplication

--- backend/filesystem_io.py
+++ backend/filesystem_io.py
@@ -12,7 +12,7 @@ def write_dataframe_to_json(filename, df):
 
 def write_json(filename, content):
     stem = settings.PRODUCTION_DOCUMENTS_OUTPUT_PATH
-    leaf = filename + '.json'
+    leaf = filename
     path = os.path.join(stem, leaf)
     print(f"Writing to production output path: {path}")
     with open(path, "w") as f:
