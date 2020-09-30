a8c45a333dc7ce1811644319d993cccdcbf6ac85
Jeanre Swanepoel <jeanre.swanepoel@gmail.com>
2020-09-30 09:48:28 +0200

    remove browser module

--- src/app/topicdetail/topicdetail.module.ts
+++ src/app/topicdetail/topicdetail.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,7 +10,6 @@ import { TopicdetailPage } from './topicdetail.page';
 
 @NgModule({
   imports: [
-    BrowserModule,
     CommonModule,
     FormsModule,
     IonicModule,
