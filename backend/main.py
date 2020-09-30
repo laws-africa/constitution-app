f159be077c474ab2d65ae99613356a7517c6bc2e
Vincent van Wyk <vincent@fluenty.co.za>
2020-09-30 15:12:45 +0200

    Resolving merge conflicts.

--- backend/main.py
+++ backend/main.py
@@ -27,16 +27,41 @@ def main():
     topics = get_topics()
     transform.write_all_documents(cases, topics)
 
-def get_cases():
-    cases = transform.read_google_sheets('cases')
-    return cases 
+def write_all_documents(cases, topics):
+    write_json(
+            JSON_FILENAME, 
+            {
+                "cases": {case[0]: case[1] for case in cases},
+                "topics": {topic[0]: topic[1] for topic in topics}
+                }
+            )
+
+def fetch_csv(content_type):
+    if content_type == "cases":
+        url = GOOGLE_SHEETS_CASES_URL
+    elif content_type == "topics":
+        url = GOOGLE_SHEETS_SECTIONS_URL
+    else:
+        raise ValueError(
+                "Please choose a valid content type sheet to read.\n"
+                f"{content_type} is not a valid selection. Choose between 'cases' and 'topics'"
+                )
+
+    df = pd.read_csv(
+        url,
+        sep=","
+        )
+    df = df.fillna("")
+    return df 
 
 
-def get_topics():
-    topics = transform.read_google_sheets('topics')
-    return topics 
-
-
+def write_json(filename, content):
+    stem = PRODUCTION_DOCUMENTS_OUTPUT_PATH
+    leaf = filename
+    path = os.path.join(stem, leaf)
+    print(f"Writing to production output path: {path}")
+    with open(path, "w") as f:
+        f.write(json.dumps(content))
 
 
 if __name__=="__main__":
