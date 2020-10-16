c87df59be81f98b7421e7bd17df94bdda21b5687
Greg Kempe <greg@kempe.net>
2020-10-16 11:14:48 +0200

    Make constitution text searchable

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as Data from "../../assets/data/data.json"
+import * as Data from "../../assets/data/data.json";
+import * as Constitution from '../../assets/data/constitution.json';
 
 @Component({
   selector: 'app-results',
@@ -10,18 +11,25 @@ import * as Data from "../../assets/data/data.json"
 })
 export class ResultsPage implements OnInit {
   data: any = (Data as any).default;
+  constitution: any = (Constitution as any).default;
+  searchableProvisions: {title: string, content: string, id: string}[];
   cases: [];
   topics: [];
-  term: String = "";
-  constructor(private router: Router, private route: ActivatedRoute, private location: Location) { }
+  provisions: {title: string, content: string, id: string}[];
+  term = '';
+
+  constructor(private router: Router, private route: ActivatedRoute, private location: Location) {
+    this.setupConstitutionSearch();
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.term = params['term'];
+      this.term = params.term;
+      const needle = this.term.toLocaleLowerCase();
 
-      this.cases = this.data.cases.filter((x) => x.title.toLowerCase().includes(params['term'])|| (x.summary ? x.summary.includes(params["term"]) : null))
-      this.topics = this.data.topics.filter((x) => x.title.toLowerCase().includes(params['term']) || (x.summary ? x.summary.includes(params["term"]) : null))
-
+      this.cases = this.data.cases.filter((x) => x.title.toLowerCase().includes(needle) || (x.summary ? x.summary.includes(needle) : null));
+      this.topics = this.data.topics.filter((x) => x.title.toLowerCase().includes(needle) || (x.summary ? x.summary.includes(needle) : null));
+      this.provisions = this.searchableProvisions.filter((x) => x.title.includes(needle) || x.content.includes(needle));
     });
   }
 
@@ -35,5 +43,29 @@ export class ResultsPage implements OnInit {
 
   navigateCaseToDetails(id: any) {
     this.router.navigateByUrl('tabs/case/detail/' + id);
+  }
+
+  /**
+   * Setup a list of provisions that we can search, which are all the sections of the constitution
+   */
+  setupConstitutionSearch() {
+    // everything that can contain searchable text
+    const selector = '.akn-p, .akn-listIntroduction, .akn-intro, .akn-wrapUp';
+    const body = new DOMParser().parseFromString(this.constitution.body, 'text/html');
+    this.searchableProvisions = [];
+
+    body.querySelectorAll('.akn-section').forEach(section => {
+      // gather text content
+      const text = [];
+      section.querySelectorAll(selector).forEach(elem => {
+        text.push(elem.textContent);
+      });
+
+      this.searchableProvisions.push({
+        title: section.querySelector('h3').textContent.toLocaleLowerCase(),
+        content: text.join(' ').toLocaleLowerCase(),
+        id: section.id
+      });
+    });
   }
 }
